ffa5960068d2238cedd8758d624f7317bc9d5f12
Keith Beckman <hcgridiron49@gmail.com>
2011-07-05 23:35:10 -0500

    GoogleAnalytics.cs comments; cleanup

--- C4SC.Common.WebControls/Google/GoogleAnalytics.cs
+++ C4SC.Common.WebControls/Google/GoogleAnalytics.cs
@@ -7,17 +7,17 @@ using System.Web.UI.WebControls;
 namespace C4SC.Common.WebControls.Google
 {
 	/// <summary>
-	/// Emits Google Analytics Javascript. Be sure to include this control on your site's MasterPage or a base page
-	/// that will ensure this Javascript gets emitted inside the HEAD element on every rendered page. 
+	/// Emits Google Analytics JavaScript. Be sure to include this control on your site's MasterPage or a base page
+	/// that will ensure this JavaScript gets emitted inside the HEAD element on every rendered page. 
 	/// </summary>
 	[DefaultProperty("AccountId")]
 	[ToolboxData("<{0}:GoogleAnalytics runat=server></{0}:GoogleAnalytics>")]
 	[ToolboxBitmap(typeof(GoogleAnalytics), @"Resources\google.bmp")]
 	public class GoogleAnalytics : WebControl
 	{
-		private const char Quote = '"';
-		private static string _googleAnalyticsJavaScript = String.Empty;
-		private static readonly object _lockObject = new object();
+		private static string _googleAnalyticsJavaScript	= String.Empty;
+		private static readonly object _lockObject			= new object();
+		private const char Quote							= '"';
 
 		/// <summary>
 		/// Google Analytics Account Id.
@@ -55,27 +55,28 @@ namespace C4SC.Common.WebControls.Google
 		{
 			return;
 		}
+
 		/// <summary>
-		/// 
+		/// Renders control's contents.
 		/// </summary>
-		/// <param name="output"></param>
+		/// <param name="output"><see cref="HtmlTextWriter"/></param>
 		protected override void RenderContents(HtmlTextWriter output)
 		{
-			output.Write(GetGoogleAnalyticsJavascript());
+			output.Write(GetGoogleAnalyticsJavaScript());
 		}
 		 
 		/// <summary>
-		/// Builds the Google Analytics Javascript block.
+		/// Builds the Google Analytics JavaScript block.
 		/// </summary>
-		/// <returns>Google Analytics Javascript block.</returns>
-		protected string GetGoogleAnalyticsJavascript()
+		/// <returns>Google Analytics JavaScript block.</returns>
+		protected string GetGoogleAnalyticsJavaScript()
 		{
 			if (_googleAnalyticsJavaScript.Length == 0)
 			{
 				lock (_lockObject)
 				{
 					_googleAnalyticsJavaScript =
-						@"<script type=" + Quote + "text/javascript" + Quote + ">" + //Environment.NewLine +
+						@"<script type=" + Quote + "text/javascript" + Quote + ">" +
 						@"	var _gaq = _gaq || [];" + Environment.NewLine +
 						@"	_gaq.push(['_setAccount', '" + AccountId + "']);" + Environment.NewLine +
 						@"	_gaq.push(['_trackPageview']);" + Environment.NewLine +
